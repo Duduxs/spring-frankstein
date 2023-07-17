90921024bc9fc7d6487074ad50808d06ae2254db
Eduardo José <duduxss3@gmail.com>
2023-07-17 14:46:13 -0300

    Creating compose file and first EP

---
+++ mongo-webflux/src/main/java/com/edudev/mongowebflux/HelloResource.java
@@ -0,0 +1,15 @@
+package com.edudev.mongowebflux;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class HelloResource {
+
+    @GetMapping
+    public Mono<String> sayHello() {
+        return Mono.just("Hello");
+    }
+
+}
