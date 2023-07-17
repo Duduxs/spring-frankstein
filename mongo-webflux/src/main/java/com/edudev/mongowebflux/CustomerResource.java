a88f3f7605f2952525e2f6e07063b1dcac0d2f5d
Eduardo José <duduxss3@gmail.com>
2023-07-17 15:20:27 -0300

    Finishing webflux project

--- mongo-webflux/src/main/java/com/edudev/mongowebflux/CustomerResource.java
+++ mongo-webflux/src/main/java/com/edudev/mongowebflux/CustomerResource.java
@@ -2,6 +2,7 @@ package com.edudev.mongowebflux;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,6 +17,14 @@ public class CustomerResource {
     @GetMapping
     public Flux<Customer> findAll() {
         return repository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Mono<ResponseEntity<Customer>> findByID(@PathVariable String id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+
     }
 
     @PostMapping
