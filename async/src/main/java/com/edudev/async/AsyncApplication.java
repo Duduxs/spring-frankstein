62c34e1a4f88e80d78a28bd71ccf0424ce2d4da8
Eduardo José <duduxss3@gmail.com>
2023-07-17 16:37:12 -0300

    Creating async project and making its configuration

---
+++ async/src/main/java/com/edudev/async/AsyncApplication.java
@@ -0,0 +1,15 @@
+package com.edudev.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+public class AsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncApplication.class, args);
+	}
+
+}
