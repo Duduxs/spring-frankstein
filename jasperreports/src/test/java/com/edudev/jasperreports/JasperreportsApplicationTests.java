34cebce4bc1790a41f47d69ad175766d96235176
Eduardo José <duduxss3@gmail.com>
2023-07-18 15:48:53 -0300

    Project created

---
+++ jasperreports/src/test/java/com/edudev/jasperreports/JasperreportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edudev.jasperreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperreportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
