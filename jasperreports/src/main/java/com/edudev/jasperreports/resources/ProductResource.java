f2ca3a6eb9e3e5705a30cd8a5614bad267c37ed4
Eduardo José <duduxss3@gmail.com>
2023-07-25 08:33:36 -0300

    Rendering my report on web

--- jasperreports/src/main/java/com/edudev/jasperreports/resources/ProductResource.java
+++ jasperreports/src/main/java/com/edudev/jasperreports/resources/ProductResource.java
@@ -1,12 +1,19 @@
 package com.edudev.jasperreports.resources;
 
 import jakarta.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.util.JRLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/products")
@@ -15,8 +22,22 @@ public class ProductResource {
     @Autowired
     private DataSource dataSource;
 
-    @PostMapping("/report")
-    public void printReport(HttpServletResponse response) {
+    @GetMapping("/report")
+    public void printReport(HttpServletResponse response) throws JRException, IOException {
+
+        var stream = getClass().getResourceAsStream("/reports/Products.jasper");
+
+        Map<String, Object> params = new HashMap<>();
+
+        JasperReport jasperReport = (JasperReport) JRLoader.loadObject(stream);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JREmptyDataSource());
+
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "inline; filename=products.pdf");
+
+        OutputStream outputStream = response.getOutputStream();
+
+        JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
 
     }
 
