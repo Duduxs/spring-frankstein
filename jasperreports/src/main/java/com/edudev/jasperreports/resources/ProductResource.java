598a83db1988886514caa6c6ec232732f9c4dd68
Eduardo José <duduxss3@gmail.com>
2023-07-24 21:40:00 -0300

    Adding new dependency jasper reports

--- jasperreports/src/main/java/com/edudev/jasperreports/resources/ProductResource.java
+++ jasperreports/src/main/java/com/edudev/jasperreports/resources/ProductResource.java
@@ -1,9 +1,23 @@
 package com.edudev.jasperreports.resources;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
 
 @Controller
 @RequestMapping("/products")
 public class ProductResource {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @PostMapping("/report")
+    public void printReport(HttpServletResponse response) {
+
+    }
+
 }
