5f1add1bb9c6d3d840472678e801ab6e448b77ea
Eduardo J <duduxss3@gmail.com>
2023-08-01 14:00:20 -0300

    Create Category done

---
+++ grpc/src/main/java/com/edudev/grpc/services/CourseCategoryService.java
@@ -0,0 +1,40 @@
+package com.edudev.grpc.services;
+
+import com.edudev.grpc.entities.Category;
+import com.edudev.grpc.protos.CategoryResponse;
+import com.edudev.grpc.protos.CategoryServiceGrpc.CategoryServiceImplBase;
+import com.edudev.grpc.protos.CreateCategoryRequest;
+import com.edudev.grpc.repositories.CategoryRepository;
+import io.grpc.stub.StreamObserver;
+import org.lognet.springboot.grpc.GRpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@GRpcService
+public class CourseCategoryService extends CategoryServiceImplBase {
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CourseCategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void createCategory(CreateCategoryRequest request, StreamObserver<CategoryResponse> responseObserver) {
+
+        var category = new Category(null, request.getName(), request.getDescription());
+        categoryRepository.save(category);
+
+        responseObserver.onNext(
+                CategoryResponse
+                        .newBuilder()
+                        .setCategory(
+                                com.edudev.grpc.protos.Category.newBuilder()
+                                        .setId(category.id.toString())
+                                        .setName(category.name)
+                                        .setDescription(category.description)
+                        ).build()
+        );
+
+        responseObserver.onCompleted();
+    }
+}
